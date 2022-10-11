9c803f5a5bf3b1dbe09ce387594dd2765161f048
Calvin <84696281+caltran117@users.noreply.github.com>
2022-10-10 22:30:16 -0400

    timeblock array

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 var today= moment();
 $("#currentDay").text(today.format("dddd MMMM DD, YYYY"));
 
+const timeblocks = [
+    "8 am",
+    "10 am",
+    "11 am",
+    "12 pm",
+    "1 pm",
+    "2 pm",
+    "3 pm",
+    "4 pm",
+    "5 pm",
+    "6 pm",
+    "7 pm",
+    "8 pm",
+    "9 pm",
+    "10 pm"
+]
+
+
+// short cut version of function element(){}
 GenerateTimeBlocks();
 GenerateTimeBlocks = () => 
     TimeBlocksList.innerHTML = "";
