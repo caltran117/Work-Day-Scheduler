5897086ae288c301f8c0151e5775bd0fc52271c7
Calvin <84696281+caltran117@users.noreply.github.com>
2022-10-10 21:29:37 -0400

    created function for timeblock

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
+
 var today= moment();
 $("#currentDay").text(today.format("dddd MMMM DD, YYYY"));
+
+GenerateTimeBlocks();
+GenerateTimeBlocks = () => 
+    TimeBlocksList.innerHTML = "";
+
+// var timeslots = document.createElement("div");
+// timeslots.textContent = `
+// #box {
+//     width: 100px;
+
+// }`
+// document.body.appendChild(timeslots);
